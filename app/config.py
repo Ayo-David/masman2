4868fc270eb6e71c25141d20dd06db0415dc4dc1
SchoolShell\Ayo-David <ayodavid.ajayi@gmail.com>
2018-03-12 09:06:58 +0100

    user page update

---
+++ app/config.py
@@ -0,0 +1,11 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess_myid'
+
+class Config(object):
+    # ...
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ from flask_jsglue import JSGlue
 
 
 app = Flask(__name__)
+app.secret_key = '****mysecret////'
 jsglue = JSGlue(app)
+
+#app.config.from_object('config')
 
 #client = MongoClient('localhost', 27017)
 #db = client.testdb#like the database
@@ -31,9 +34,10 @@ def register(report=None):
         user = db.user
         generation1 = db.generation1
         generation2 = db.generation2
+        generation3 = db.generation3
 
 
-        existing_user = user.find_one({'username' : request.form['name']})
+        existing_user = user.find_one({'username' : request.form['username']})
 
         referral_id = request.form['referral_id'].upper()
 
@@ -48,15 +52,22 @@ def register(report=None):
         #to fetch a grand parent
         grand_parent = generation1.find_one({'user_id' : rp})
 
+        grand_parent_refid = grand_parent['referral_id']
+
+        grand_parent_refid =  grand_parent_refid[4:]
+
+        gp = int(grand_parent_refid)
+
         #print (great grand_parent)
-        #gg_parent = generation2.find_one({'user_id' : grand_parent['referral_id']})
+        gg_parent = generation1.find_one({'user_id' : gp})
+        gps = gg_parent['referral_id']
        
         #report = 'Grand Parent' + grand_parent + 'Referal Parent' + str(referral_parent)
  
         #check if referral exist
         if existing_referral:
 
-            user_id = user.find().count()+1
+            user_id = user.find().count()+18
             referrer_id = 'MFNG'+ str(user_id)
             #referrer_id is the id the user can give to others to refer them to the platform
             #referral_id is the id of the person who refer you
@@ -72,7 +83,7 @@ def register(report=None):
             
             #hashpassword = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
                 user.insert({
-                    'user_id':user.find().count()+1, #auto increment in the db
+                    'user_id':user.find().count()+18, #auto increment in the db
                     'referrer_id':referrer_id,
                     'name':request.form['name'],
                     'username':request.form['username'], 
@@ -81,7 +92,6 @@ def register(report=None):
                     'phone':request.form['phone'], 
                     'whatsapp_no':request.form['whatsapp_no'],
                     'address':request.form['address'], 
-                    'email':request.form['email'],
                     'nationality':request.form['nationality'], 
                     'next_of_kin':request.form['next_of_kin'], 
                     'next_phone':request.form['next_phone'],
@@ -94,29 +104,30 @@ def register(report=None):
                     'referral_name':request.form['referral_name'],
                     'referral_id':referral_id
                     })
-
+                
+                #if not within the 1st 12 (the first 12 will not have a referal) 
                 if user_id>12:
 
                     generation1.insert({
                         'user_id':user_id, #auto increment in the db
-                        'referral_id':referral_id
+                        'referral_id':referral_id #this is the parent of the user_id
                         })
 
 
                     if grand_parent:
                         generation2.insert({
                             'user_id':user_id, #auto increment in the db
-                            'referral_id':referral_id,
-                            'grand_parent':grand_parent['referral_id']
+                            'referral_id':referral_id,#this is the parent of the user
+                            'grand_parent':grand_parent['referral_id'] #this is the grand-parent of the user
                             })
 
-                        # if gg_parent:
-                        #     generation3.insert({
-                        #         'user_id':user_id, #auto increment in the db
-                        #         'referral_id':referral_id,
-                        #         'grand_parent':grand_parent['referral_id']
-                        #         'gg_parent':gg_parent['referral_id']
-                        #         })
+                        if gg_parent:
+                            generation3.insert({
+                                'user_id':user_id, #auto increment in the db
+                                'referral_id':referral_id,
+                                'grand_parent':grand_parent['referral_id'],
+                                'gg_parent':gg_parent['referral_id']
+                                })
                                 
 
                 report =  'Successfully Register. You can now Login'
@@ -125,7 +136,7 @@ def register(report=None):
                 report = 'Username is not available, please try another username'
  
         else:
-            #report = 'Grand Parent' + str(grand_parent) + 'Referal Parent' + str(referral_parent)
+            #report = 'GG:'+ str(gps) + 'Grand Parent' + str(grand_parent_refid) + 'Referal Parent' + str(referral_parent)
             report = 'The referrer with REFERRAL ID:' + str(referral_id) + ', does not exist. Please confirm the ID with your referrer'
 
     return render_template('register.html', report = report)
@@ -165,12 +176,18 @@ def userdashboard():
     #if session is set
     if g.user:                                                                                                                                                             
         user = db.user
+        generation1 = db.generation1
+        generation2 = db.generation2
+        generation3 = db.generation3
+        
         login_user = user.find_one({'username' : session['username']})
         myid = 'MFNG' + str(login_user['user_id'])
        
         #return render_template('userdashboard.html', user=session['email'])
         downline1 = user.find({'referral_id':myid})
-        return render_template('referral.html', downline = downline1, user=session['username'])
+        downline2 = generation2.find({'grand_parent':myid})
+        downline3 = generation3.find({'gg_parent':myid})
+        return render_template('referral.html', downline = downline1, downline2 = downline2, downline3 = downline3, user=session['username'], myid = myid)
 
     return render_template('login.html', report = report)   
 
@@ -189,7 +206,7 @@ def before_request():
 @app.route('/logout')
 def dropsession():
     session.pop('username', None)
-    repor = 'You are Successfully logged out'
+    report = 'You are Successfully logged out'
     
 
 @app.route('/insert')
@@ -208,7 +225,6 @@ def find():
     
 
 if __name__ == "__main__":
-    app.secret_key = '****mysecret////'
     app.run(debug = True)
     
     
